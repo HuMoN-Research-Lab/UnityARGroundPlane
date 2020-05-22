8e8898658704b3848fea91695a2d13304f6bf41b
StephenReinhart <Stephen.SER@outlook.com>
2020-05-22 11:03:06 -0400

    Foot testing
    Model foot is present, strange unity package error stopping progress

--- Assets/XRExperimentStructure/Experiment 1/Scripts/ControlLevel_Main.cs
+++ Assets/XRExperimentStructure/Experiment 1/Scripts/ControlLevel_Main.cs
@@ -29,7 +29,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-*/
+
 
 using System.Collections;
 using System.Collections.Generic;
@@ -77,3 +77,5 @@ public class ControlLevel_Main : ControlLevel {
         goodbye.AddTimer(2f, null);
     }
 }
+
+*/
