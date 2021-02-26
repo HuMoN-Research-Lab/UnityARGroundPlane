f31e3da0bb08a9da2c09f2a1bbbc49d3e8e626e5
StephenReinhart <Stephen.SER@outlook.com>
2021-02-26 10:16:10 -0500

    Merge
    Wilson also bounces now when you press M (short for intentional [m]iss)

--- Assets/____TESTBED/ballmove.cs
+++ Assets/____TESTBED/ballmove.cs
@@ -13,17 +13,22 @@ public class ballmove : MonoBehaviour
 
     float moveHorizontal = 0f;
     float moveVertical = 0f;
+    bool isDipping = false;
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.W)) moveVertical = 1;
         else if (Input.GetKeyDown(KeyCode.S)) moveVertical = -1;
         if (Input.GetKeyDown(KeyCode.A)) moveHorizontal = -1;
         else if (Input.GetKeyDown(KeyCode.D)) moveHorizontal = 1;
+        if (Input.GetKeyDown(KeyCode.M)) isDipping = true;
 
         if (Input.GetKeyUp(KeyCode.W)) moveVertical = 0;
         if (Input.GetKeyUp(KeyCode.A)) moveHorizontal = 0;
         if (Input.GetKeyUp(KeyCode.S)) moveVertical = 0;
         if (Input.GetKeyUp(KeyCode.D)) moveHorizontal = 0;
+        if (Input.GetKeyUp(KeyCode.M)) isDipping = false;
+
+        
     }
 
     void FixedUpdate()
@@ -31,6 +36,8 @@ public class ballmove : MonoBehaviour
         Vector3 movement = new Vector3 (moveVertical, 0.0f, -moveHorizontal);
 
         rb.velocity = (movement * speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, (isDipping ? 0.098f : 0.102f), transform.position.z);
     }
 
 }
