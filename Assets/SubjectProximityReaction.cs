15dce1555b330f9528ae0b8d761da114e8360ec4
StephenReinhart <stephenreinhart29@gmail.com>
2022-01-26 13:40:45 -0500

    Prototype C Proximity Script
    Still needs to be placed correctly in the center projection

---
+++ Assets/SubjectProximityReaction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using QualisysRealTime.Unity;
+
+public class SubjectProximityReaction : MonoBehaviour
+{
+    public GameObject movingSubject;
+
+    public Material black;
+    public Material red;
+
+    public double proximity = .5; //meters
+
+    void Update() {
+        Vector2 self2D = new Vector2(this.transform.position.x, this.transform.position.z);
+        Vector2 mSub2D = new Vector2(movingSubject.transform.position.x, movingSubject.transform.position.z);
+        float dist2D = Vector2.Distance(self2D, mSub2D);
+
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+
+        if(dist2D < proximity) {
+            //set red
+            mr.material = red;
+        } else {
+            //set black
+            mr.material = black;
+        }
+    }
+    
+}
